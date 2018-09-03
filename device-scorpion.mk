7aefebb347e6f7c6a4e3410d5a974008b904687f
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    redbull: Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- device-scorpion.mk
+++ device-scorpion.mk
@@ -11,6 +11,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-scorpion
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
